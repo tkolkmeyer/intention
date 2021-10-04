a2c9a3075cd32a6cce221e85da22775808c3adad
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-04 09:50:48 -0500

    color wheel

--- default.js
+++ default.js
@@ -1,3 +1,18 @@
 function spinLogo(x) {
     x.classList.add("spin");
 }
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+let image = ctx.createImageData(100, 100);
+let data = image.data;
+
+let length = 100 * 100 * 4;
+for (let i = 0; i < length; i += 4) {
+    data[i] = 255; // full red
+    data[i + 1] = 0; // no green
+    data[i + 2] = 0; // no blue
+    data[i + 3] = 255; // fully opaque
+}
+
+ctx.putImageData(image, 0, 0);
