25fc1080ecdb0f273ebb689fa37ef78305629318
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-04 10:28:05 -0500

    typo fix

--- sketch.js
+++ sketch.js
@@ -4,5 +4,7 @@ function setup() { 
 }
 
 function draw() {  
-    ellipse(mouseX, mouseY, mouseY / 3)   let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000))  fill(c)
+    ellipse(mouseX, mouseY, mouseY / 3)  
+    let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000)) 
+    fill(c)
 }
