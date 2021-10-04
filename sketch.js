b60553c30ab855eb33602cf9b4ef68c49aa7caf7
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-04 10:20:48 -0500

    p5 addition

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() { 
+    createCanvas(800, 800); 
+    background(220);
+}
+
+function draw() {  
+    ellipse(mouseX, mouseY, mouseY / 3)   let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000))  fill(c)
+}
