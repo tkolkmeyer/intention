9a217136ce71d61bedfe2827b9797ffd9346e1b3
Jake Howell <jakehowellphoto@gmail.com>
2021-10-06 23:53:37 -0500

    triangle

--- app/sketch.js
+++ app/sketch.js
@@ -1,11 +1,11 @@
 function setup() { 
     createCanvas(1000, 600); 
     background(51); //TODO@lavalord changed the background color
-    
+
 }
 
 function draw() {  
-    ellipse(mouseX, mouseY, mouseY / 3)  
+    triangle(mouseX, mouseY, mouseY / 3)  
     let c = color(mouseX, mouseY, Math.floor(Math.random() * 1000)) 
     fill(c)
 }
